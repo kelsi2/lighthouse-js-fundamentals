9508afcba456fc74e15dc6d34e1ba6df692ab967
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-07-19 04:18:30 +0000

    Practice with return vs. console.log

--- hello.js
+++ hello.js
@@ -1,11 +1,23 @@
 /*const sayHello = function () { //declaring function in a variable called sayHello
   console.log("Hello, world"); // function sayHello prints Hello world to the console
 }
-sayHello(); //calling sayHello function */
+sayHello(); //calling sayHello function 
 
 const sayHello = function (name) { //improving on previous function by adding a value as input, called name
   console.log("Hello, " + name); //concatenate function input name (parameter) to string "Hello, "
 }
-sayHello("Caliban");
+sayHello("Caliban"); //don't need to rewrite greeting code every time, now can just input name to the function
 sayHello("Miranda");
-sayHello("Ferdinand");
+sayHello("Ferdinand");*/
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John"); //outputs "Hello, John" to console
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John"); //returns "Hello, John" to variable and outputs nothing to the console
+console.log(greeting); //returns can be printed to console later, same result as calling previous function (sayHelloToConsole)
+
