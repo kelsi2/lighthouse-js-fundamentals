1a72bb68f7e81d23aca858881184c8732a57d72b
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-07-20 01:41:15 +0000

    practicing arrays using ice cream flavors

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer"); //adding root beer flavor to array
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]); //writing first array index to the console
+
+console.log(iceCreamFlavours[5]); //accessing last item in array (root beer)
+
+console.log(iceCreamFlavours.length); //writing total number of items in array
