abb36cc21dd201b16e282694b3960009e97c8f1e
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-07-20 01:44:38 +0000

    updated file to include while loop example

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+for (let i = 0; i < packingList.length; i++) { //for loop
   console.log(packingList[i]);
+}
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) { //while loop (same result as for loop but uses extra lines)
+  console.log(packingList[i]);
+  i++;
 }
